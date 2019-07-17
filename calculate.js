389c264f70514701e0d3651b11479ed102492af2
rubentrottein <38758157+rubentrottein@users.noreply.github.com>
2019-07-17 11:11:32 +0200

    min don't work: why?

--- calculate.js
+++ calculate.js
@@ -19,3 +19,14 @@ console.log(calculer(4, "+", 6));  // 10
 console.log(calculer(4, "-", 6));  // -2
 console.log(calculer(2, "*", 0));  // 0
 console.log(calculer(12, "/", 0)); // Infinity
+
+const min = (nombreA, nombreB) => {
+  if (`${nombreA}` < `${nombreB}`){
+    return `${nombreA}`;
+  } else {
+    return `${nombreB}`;
+  }
+}
+console.log(min(4.5, 5)); // 4.5
+console.log(min(19, 9));  // 9
+console.log(min(1, 1));   // 1
